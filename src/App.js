3ed1f2a7af301c55a127c1c6fbee576ab8ef8a5d
Janelle Kiker <janellekiker@gmail.com>
2022-12-13 16:35:02 -0600

    footer added, completed code minus imgs

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import NavBar from "./components/NavBar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./components/pages/Home";
+import Services from "./components/pages/Services";
+import Products from "./components/pages/Products";
+import SignUp from "./components/pages/SignUp";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -11,7 +15,11 @@ function App() {
         <NavBar />
         <Routes>
           <Route path="/" exact element={<Home />} />
+          <Route path="/services" exact element={<Services />} />
+          <Route path="/products" exact element={<Products />} />
+          <Route path="/sign-up" exact element={<SignUp />} />
         </Routes>
+        <Footer />
       </Router>
     </div>
   );
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -11,16 +11,41 @@ function Card() {
         <div className="cards__wrapper">
           <ul className="cards__items">
             <CardItem
-              src={"/assets/images/waterfall.jpg"}
+              // TODO fix image link
+              src={"images/img-9.jpg"}
               text="Explore the hidden waterfall deep inside the Amazon Jungle"
               label="Adventure"
               path="/services"
             />
             <CardItem
+              // TODO fix image link
               src={"images/img-2.jpg"}
               text="Travel through the islands of Bali in a private cruise"
               label="Luxury"
               path="/services"
+            />
+          </ul>
+          <ul className="cards__items">
+            <CardItem
+              // TODO fix image link
+              src={"images/img-3.jpg"}
+              text="Set sail in the Atlantic Ocean visiting uncharted waters"
+              label="Mystery"
+              path="/services"
+            />
+            <CardItem
+              // TODO fix image link
+              src={"images/img-4.jpg"}
+              text="Experience football on top of the Himilayan Mountains"
+              label="Adventure"
+              path="/products"
+            />
+            <CardItem
+              // TODO fix image link
+              src={"images/img-8.jpg"}
+              text="Ride through the Sahara Desert on a guided camel tour"
+              label="Adventure"
+              path="/sign-up"
             />
           </ul>
         </div>
