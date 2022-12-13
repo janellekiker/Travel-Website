32b92afc32d38bd72ba96398edeae26a37bee755
Janelle Kiker <janellekiker@gmail.com>
2022-12-13 17:38:30 -0600

    finished code

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,7 +1,11 @@
 import React from "react";
 import CardItem from "./CardItem";
 import "./Cards.css";
-//import waterfall from "../assets/images/waterfall.jpg";
+import image9 from "../assets/images/img-9.jpg";
+import image2 from "../assets/images/img-2.jpg";
+import image3 from "../assets/images/img-3.jpg";
+import image4 from "../assets/images/img-4.jpg";
+import image8 from "../assets/images/img-8.jpg";
 
 function Card() {
   return (
@@ -12,14 +16,14 @@ function Card() {
           <ul className="cards__items">
             <CardItem
               // TODO fix image link
-              src={"images/img-9.jpg"}
+              src={image9}
               text="Explore the hidden waterfall deep inside the Amazon Jungle"
               label="Adventure"
               path="/services"
             />
             <CardItem
               // TODO fix image link
-              src={"images/img-2.jpg"}
+              src={image2}
               text="Travel through the islands of Bali in a private cruise"
               label="Luxury"
               path="/services"
@@ -28,21 +32,21 @@ function Card() {
           <ul className="cards__items">
             <CardItem
               // TODO fix image link
-              src={"images/img-3.jpg"}
+              src={image3}
               text="Set sail in the Atlantic Ocean visiting uncharted waters"
               label="Mystery"
               path="/services"
             />
             <CardItem
               // TODO fix image link
-              src={"images/img-4.jpg"}
+              src={image4}
               text="Experience football on top of the Himilayan Mountains"
               label="Adventure"
               path="/products"
             />
             <CardItem
               // TODO fix image link
-              src={"images/img-8.jpg"}
+              src={image8}
               text="Ride through the Sahara Desert on a guided camel tour"
               label="Adventure"
               path="/sign-up"
